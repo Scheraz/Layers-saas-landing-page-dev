422a134436aa521cbca54dc26475f76b005aa701
Scheraz <sherazkhanpk.sk@gmail.com>
2025-10-20 22:04:58 +0800

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import Integrations from "@/sections/Integrations";
 import Introduction from "@/sections/Introduction";
 import LogoTicker from "@/sections/LogoTicker";
 import Navbar from "@/sections/Navbar";
+import Faq from "@/sections/Faqs";
+import CallToAction from "@/sections/CallToAction";
+import Footer from "@/sections/Footer";
 
 export default function Home() {
     return (
@@ -14,6 +17,9 @@ export default function Home() {
             <Introduction />
             <Features />
             <Integrations />
+            <Faq />
+            <CallToAction />
+            <Footer />
         </>
     );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,35 @@
-// const footerLinks = [
-//     { href: "#", label: "Contact" },
-//     { href: "#", label: "Privacy Policy" },
-//     { href: "#", label: "Terms & Conditions" },
-// ];
+import { footer, section } from "framer-motion/client";
+import logoImage from "@/assets/images/logo.svg";
+import Image from "next/image";
 
-// export default function Footer() {
-//     return <div>Footer</div>;
-// }
+const footerLinks = [
+    { href: "#", label: "Contact" },
+    { href: "#", label: "Privacy Policy" },
+    { href: "#", label: "Terms & Conditions" },
+];
+
+export default function Footer() {
+    return (
+        <section>
+            <div className="container">
+                <div className="flex flex-col justify-center items-center gap-6 md:flex-row md:justify-between">
+                    <div>
+                        <Image src={logoImage} alt="logo image" />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a
+                                    href={link.href}
+                                    className="text-white/50 text-sm"
+                                >
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
